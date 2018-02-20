4454ecd5402a3584b53660c73ce330efb9ba4737
Jim McAndrew <jim@loc8.us>
2018-02-19 22:17:52 -0500

    Fix typo

--- scripts/reset_docker.sh
+++ scripts/reset_docker.sh
@@ -1,3 +1,3 @@
 sudo docker rmi $(sudo docker images -aq) -f && sudo docker rm $(sudo docker ps -aq) -f
 sudo rm -rf ./postgres/data && mkdir -p ./postgres/data
-sudo rm -if ./tools/mnt && mkdir -p ./tools/mnt
+sudo rm -rf ./tools/mnt && mkdir -p ./tools/mnt
